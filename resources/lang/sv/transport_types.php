315ad0dcd97fd0b116b7561af1105b921b0eef93
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 20:32:24 +0100

    added Swedish

---
+++ resources/lang/sv/transport_types.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    "bus" => "buss",
+    "business" => "affärsresa",
+    "businessPlural" => "Affärsresor",
+    "express" => "järrtrafik",
+    "national" => "fjärrtrafik",
+    "nationalExpress" => "fjärrtrafik",
+    "ferry" => "Färja",
+    "private" => "privat resa",
+    "privatePlural" => "privata resor",
+    "regional" => "Regional",
+    "suburban" => "S-Tåg",
+    "subway" => "T-bana",
+    "tram" => "Spårvagn",
+];
