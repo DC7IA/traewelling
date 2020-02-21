c8c0df7e01ed8cdf08b7d571579eff745e469eed
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:51:31 +0100

    added Swedish

---
+++ resources/lang/sv/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "password" => "Lösenord måste vara minst sex tecken långa och matcha lösenordsbekräftelsen.",
+    "reset" => "Ditt lösenord har återställts.",
+    "sent" => "Vi har skickat en länk för att återställa ditt lösenord.",
+    "token" => "Den här länken för att återställa ett lösenord är ogiltig.",
+    "user" => "Vi kunde inte hitta en användare med den här e-postadressen."
+];
