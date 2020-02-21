42be2dbc6462895a4d0dc4eb29e8abec697c70d4
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:24:15 +0100

    added Swedish

---
+++ resources/lang/sv/events.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    "header" => "Evenemang: :name",
+    "name" => "Namn",
+    "hashtag" => "Hashtag",
+    "website" => "Websida",
+    "host" => "arrangör",
+    "url" => "extern websida",
+    "begin" => "början",
+    "end" => "slut",
+    "closestStation" => "Nästa Träwelling station",
+    "on-my-way-to" => "På väg till: :name",
+    "on-my-way-dropdown" => "Jag är på väg till:",
+    "no-event-dropdown" => "(inget evenemang)",
+    "on-your-way" => "Du är på väg till <a href=':url'>:name</a>.",
+    "upcoming" => "Evenemanger i framtiden",
+    "live" => "aktuella evenemanger",
+    "past" => "tidigare evenemanger",
+    "new" => "skapa evenemang"
+];
