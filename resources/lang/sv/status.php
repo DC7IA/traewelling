39c69595af6e3032a0ac9d68e54894bba553ed2c
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 20:27:58 +0100

    added Swedish

---
+++ resources/lang/sv/status.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "ogp-title" => "Reise von :name mit Träwelling",
+    "ogp-description" => ":distancekm från :origin till :destination i :linename.|:distancekm från :origin till :destination i linje :linename."
+];
