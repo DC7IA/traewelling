85620db54512d74cd0cc6050f9b069c34fb76a15
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:47:59 +0100

    added Swedish

---
+++ resources/lang/sv/modals.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "delete-confirm" => "Radera",
+    "deleteStatus-title" => "Är du säker på att du vill radera den här statusen?",
+    "edit-confirm" => "Spara",
+    "editStatus-label" => "Redigera statustext",
+    "editStatus-title" => "redigera status",
+    "deleteEvent-title" => "Radera evenemang",
+    "deleteEvent-body" => "Är du säker på att du vill radera evenemangen <em>:name </em>? Incheckningar på väg kör sedan till <code>NULL</code>."
+];
