5036bbbc2079e79d1a6c4235e02af77be6d600f5
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:57:51 +0100

    added Swedish

---
+++ resources/lang/sv/privacy.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "title" => "Privatlivs&shy;betingelser",
+    "not-signed-yet" => "<b>Du har ännu inte godkänt vår integritetspolicy.</b> Detta krävs dock för att använda Träwelling. Om du inte samtycker kan du också ta bort ditt konto här.",
+    "we-changed" => "<b>Vi har ändrat vår integritetspolicy.</b> För att fortsätta använda den här tjänsten måste du godkänna de nya villkoren. Om du inte samtycker kan du också radera ditt konto. ",
+    "sign" => "Okej"
+];
