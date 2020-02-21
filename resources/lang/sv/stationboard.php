3c36116532c409c4f19e31a4bbca02a5bb54d196
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 20:24:51 +0100

    added Swedish

---
+++ resources/lang/sv/stationboard.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "arr" => "till",
+    "btn-checkin" => "Checka in!",
+    "check-business" => "affärsresa",
+    "check-toot" => "toot",
+    "check-tweet" => "tweet",
+    "dep" => "från",
+    "dep-time" => "avgång",
+    "destination" => "destination",
+    "dt-picker" => "tidväljare",
+    "filter-products" => "filter",
+    "label-message" => "statusmeddelande:",
+    "line" => "linje",
+    "minus-15" => "-15 minuter",
+    "new-checkin" => "ny check-in",
+    "plus-15" => "+15 minuter",
+    "set-time" => "ställa in tid",
+    "no-departures" => "Tyvärr finns det för närvarande inga avgångar för den här tiden / filtret.",
+    "station-placeholder" => "hållplatsen",
+    "stop-cancelled" => "stopp utgår",
+    "stopover" => "hållplats",
+    "submit-search" => "sök",
+    "where-are-you" => "Var är du?",
+    "last-stations" => "sista stationer",
+    "next-stop" => "Nästa stopp"
+];
