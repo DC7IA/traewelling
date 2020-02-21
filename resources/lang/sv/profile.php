c954c50985c2d4b6077eeb27eb00e7db891ac8d1
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 20:01:21 +0100

    added Swedish

---
+++ resources/lang/sv/profile.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "follow" => "Följ",
+    "last-journeys-of" => "Senaste resor från",
+    "points-abbr" => "Poäng",
+    "settings" => "Inställningar",
+    "statistics-for" => "Statistiker åt",
+    "unfollow" => "Följa inte längre"
+];
