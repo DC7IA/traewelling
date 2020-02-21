b7df8bb292106da08068c5ff8cf87b10204e4cb7
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:32:51 +0100

    added Swedish

---
+++ resources/lang/sv/leaderboard.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "distance" => "Reseavståndet",
+    "duration" => "Resans varaktighet",
+    "friends" => "Vännder",
+    "points" => "Poänger",
+    "rank" => "Rang",
+    "top20" => "Top 20",
+    "user" => "Namn"
+];
