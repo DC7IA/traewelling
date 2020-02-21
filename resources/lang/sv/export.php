e767e1611dd1cd68637ac9f997ea7083871369ba
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:30:57 +0100

    added Swedish

---
+++ resources/lang/sv/export.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    "begin" => "Början",
+    "btn" => "Exportera",
+    "end" => "Till",
+    "lead" => "Här kan du exportera dina tågresor från databasen som CSV, JSON och som PDF. De enskilda datumen är separerade från varandra med en tabulatorstopp.",
+    "submit" => "Exportera som .pdf",
+    "title" => "Exportera",
+    "type" => "kategori tåg",
+    "number" => "Tågnummer",
+    "origin" => "Avgångsstation",
+    "departure" => "Avgångstid",
+    "destination" => "Ankomststation",
+    "arrival" => "Ankomsttid",
+    "duration" => "Restid",
+    "kilometers" => "Kilometer",
+    "export" => "Export",
+    "guarantee" => "Skapad med <a href=\":url\">:name</a>. All information utan garanti.",
+    "page" => "Sida"
+];
