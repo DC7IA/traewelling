9e264f3f8e7933fff2b0a091dd593dbdf08c3e37
Joshua Hoffmann | DC7IA <spam-github@dc7ia.eu>
2020-02-21 19:39:25 +0100

    added Swedish

---
+++ resources/lang/sv/menu.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    "abort" => "Avbryta",
+    "about" => "Om Träwelling",
+    "active" => "På väg",
+    "blog" => "Blog",
+    "dashboard" => "Dashboard",
+    "developed" => "Utvecklad med <i class=\" fas fa-heart fa-sm\"style=\" color: Tomato;\"\"></i>i Europeiska Unionen.  <a href='https://github.com/traewelling/traewelling'>Quellcode</a> källkoden </a> är licensierad under <a href='https://www.gnu.org/licenses/agpl-3.0.html'>AGPLv3</a>.",
+    "discard" => "Avbryta",
+    "export" => "Exportera",
+    "globaldashboard" => "Global Dashboard",
+    "gohome" => "Till hemsidan",
+    "imprint" => "Imprint",
+    "leaderboard" => "Top-Träweller",
+    "login" => "Logga in",
+    "logout" => "Logga ut",
+    "ohno" => "Åh nej!",
+    "privacy" => "integritet",
+    "profile" => "profil",
+    "register" => "Registrera",
+    "settings" => "Inställningar"
+];
